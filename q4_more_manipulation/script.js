b7dee75e9b83f0cc2e0fe8ad49a3b64904841248
jarednjk <jarednjk@gmail.com>
2022-05-24 07:40:56 +0000

    solved qn4

---
+++ q4_more_manipulation/script.js
@@ -0,0 +1,15 @@
+document.querySelector('h1').style.color = 'green';
+
+document.querySelector('li.finished').innerText = 'Repay credit card debt';
+
+document.querySelector('li#urgent').style.border = '2px solid black';
+
+document.querySelector('.emphasis').style.textDecoration = 'underline';
+
+document.querySelector('p.emphasis').style.backgroundColor = 'orange';
+document.querySelector('p.emphasis').style.fontSize = '32px';
+document.querySelector('p.emphasis').style.lineHeight = '24px';
+
+document.querySelector('#urgent .todo').style.backgroundColor = 'yellow';
+
+document.querySelector('h2').innerHTML = '<span class="greetings">About Us</span>';
