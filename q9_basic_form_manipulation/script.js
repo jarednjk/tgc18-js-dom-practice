6039d3c4d785560c5ae0958db85fe9ac3c993098
jarednjk <jarednjk@gmail.com>
2022-05-24 09:24:51 +0000

    solve qn9

--- q9_basic_form_manipulation/script.js
+++ q9_basic_form_manipulation/script.js
@@ -2,12 +2,27 @@
 function getFormValues() {
 
     let email = null;
+    email = document.querySelector('#email-address').value;
 
     let model = null;
+    let models = document.querySelectorAll('.phone-type')
+    for (let m of models) {
+        if (m.checked) {
+            model = m.value;
+            break;
+        }
+    }
 
-    let services = null;
+    let services = [];
+    let service = document.querySelectorAll('.services')
+    for (let s of service) {
+        if (s.checked) {
+            services.push(s.value)
+        }
+    }
 
     let pickup = null;
+    pickup = document.querySelector('#pick-up').value;
 
     // leave this line alone!
     return [email, model, services, pickup];
