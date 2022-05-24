238e6ae7897b3918ab139cb12f678f3b4d2af0f7
jarednjk <jarednjk@gmail.com>
2022-05-24 07:25:11 +0000

    complete qns3

--- q3_manipulating_dom_elements/script.js
+++ q3_manipulating_dom_elements/script.js
@@ -1,5 +1,13 @@
 // PLACE YOUR CODE HERE
+let header = document.querySelector('h1');
+header.innerText = 'Hello world!';
 
+let important = document.querySelector('span#important');
+important.style.backgroundColor = 'red';
+
+let todo = document.querySelector('li.todo');
+todo.style.fontFamily = 'verdana';
+todo.style.fontSize = '16px';
 
 // LEAVE THE BELOW ALONE! DON'T TOUCH
 try {
