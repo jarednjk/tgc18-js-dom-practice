b887da79dada184201031cd7f02d2ed09aff313e
jarednjk <jarednjk@gmail.com>
2022-05-24 08:55:21 +0000

    solve qn8

--- q8_manipulating_dom_elements/script.js
+++ q8_manipulating_dom_elements/script.js
@@ -1,3 +1,22 @@
 // PLACE YOUR CODE HERE
 
 
+let emphasis = document.querySelectorAll('.emphasis');
+for (let e of emphasis) {
+    e.style.fontFamily = 'Verdana';
+}
+
+let finished = document.querySelectorAll('li.finished');
+for (let f of finished) {
+    f.style.textDecoration = 'line-through';
+}
+
+let todo = document.querySelectorAll('#low-priority .todo');
+for (let t of todo) {
+    t.style.backgroundColor = 'blue';
+}
+
+let alert = document.querySelectorAll('.alert');
+for (let a of alert) {
+    a.style.color = 'red';
+}
