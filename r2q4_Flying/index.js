237c32c2d79f6ec35e36f636083fa54c7fd90295
Jared Ng <jarednjk@gmail.com>
2022-05-28 21:32:47 +0800

    solve r2q4

--- r2q4_Flying/index.js
+++ r2q4_Flying/index.js
@@ -1,10 +1,9 @@
-function extractFormInfo()
-{
-  
+function extractFormInfo() {
+
   let firstName = document.querySelector('#first-name').value;
-  
+
   let lastName = document.querySelector('#last-name').value;
-  
+
   // let ticketClass = document.querySelector('input[name="ticket-class"]:checked');
 
   let ticketClass = null;
@@ -15,7 +14,7 @@ function extractFormInfo()
       break;
     }
   }
-  
+
   let optionsArray = [];
   let options = document.querySelectorAll('.options');
   for (let o of options) {
@@ -29,7 +28,7 @@ function extractFormInfo()
 
 
 
-document.querySelector('#btn-book').addEventListener('click', function(){
+document.querySelector('#btn-book').addEventListener('click', function () {
   let infoArray = extractFormInfo();
   let [firstName, lastName, ticketClass, optionsArray] = infoArray;
   console.log(firstName);
@@ -37,13 +36,34 @@ document.querySelector('#btn-book').addEventListener('click', function(){
   console.log(ticketClass);
   console.log(optionsArray);
 
-  let liElement = document.createElement('li');
-  for(info of infoArray) {
+  let div = document.querySelector('#summary');
+  
+  for (info of infoArray) {
+    let liElement = document.createElement('li');
     liElement.innerHTML = info;
-    
+    div.appendChild(liElement);
   }
- 
+  
+  let costElement = document.createElement('li');
+  let cost = 0;
+  
+  if (ticketClass == 'first-class') {
+    cost += 1200;
+  } else if (ticketClass == 'business-class') {
+    cost += 750;
+  } else if (ticketClass == 'economy') {
+    cost += 250;
+  }
 
-  let summary = document.querySelector('#summary');
+  let options = document.querySelectorAll('.options');
+  for (let o of options) {
+    if (o.checked) {
+      cost += 10;
+    }
+  }
+  
+  costElement.innerHTML = cost;
+
+  div.appendChild(costElement);
 
 })
